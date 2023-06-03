c49e13b52d11eb5e649318e6daba48975269a379
luqui <playlucas273@gmail.com>
2023-06-02 22:20:11 -0300

    .

--- test.py
+++ test.py
@@ -1,51 +1,44 @@
+import random
 import math
 import matplotlib.pyplot as plt
 
 
-class MishraBirdMountain:
+class RandomMountain:
     def __init__(self, visual_radius, base_radius):
-        flag = [-3.1302468, -1.5821422]
-        flag[0] = flag[0] * base_radius / 5
-        flag[1] = flag[1] * base_radius / 5
-        flag[0] = flag[0] - 5
-        flag[1] = flag[1] - 5
-        flag = tuple(flag)
-        self.flag = flag
         self.visual_radius = visual_radius
         self.base_radius = base_radius
+        self.functions = [
+            lambda x, y: x**2 + y**2,
+            lambda x, y: x**3 + y**3,
+            lambda x, y: math.sin(x) + math.cos(y),
+            lambda x, y: math.exp(x) - math.exp(y)
+        ]
 
-    def mishra_bird_function(self, x: float, y: float) -> float:
-        x = x / self.base_radius * 5
-        y = y / self.base_radius * 5
-        x = x - 5
-        y = y - 5
-        f = math.sin(y) * math.exp((1 - math.cos(x**2)) ** 2) + math.cos(x) * \
-            math.exp((1 - math.sin(y)) ** 2) + (x - y) ** 2 - 100
-        return -f
+    def random_mountain_function(self):
+        num_functions = random.randint(1, len(self.functions))
+        selected_functions = random.sample(self.functions, num_functions)
+        scalars = [random.uniform(0.5, 2.0) for _ in range(num_functions)]
 
-    def mishra_bird_function_gradient(self, x: float, y: float) -> tuple:
-        x = x / self.base_radius * 5
-        y = y / self.base_radius * 5
-        x = x - 5
-        y = y - 5
-        dfdx = 2 * (x - y) + math.sin(y) * math.exp((1 - math.cos(x)) ** 2) * 2 * \
-            (1 - math.cos(x)) * math.sin(x) - \
-            math.sin(x) * math.exp((1 - math.sin(y)) ** 2)
-        dfdy = 2 * (y - x) - math.cos(x) * math.exp((1 - math.sin(y)) ** 2) * 2 * \
-            (1 - math.sin(y)) * math.cos(y) + \
-            math.cos(y) * math.exp((1 - math.cos(x)) ** 2)
-        return -dfdx, -dfdy
+        def mountain_function(x, y):
+            result = 0
+            for func, scalar in zip(selected_functions, scalars):
+                result += scalar * func(x, y)
+            return result
+
+        return mountain_function
 
     def plot_mountain(self):
         x = []
         y = []
         z = []
 
-        for i in range(-self.base_radius, self.base_radius + 1):
-            for j in range(-self.base_radius, self.base_radius + 1):
+        mountain_function = self.random_mountain_function()
+
+        for i in range(-self.visual_radius, self.visual_radius + 1):
+            for j in range(-self.visual_radius, self.visual_radius + 1):
                 x.append(i)
                 y.append(j)
-                z.append(self.mishra_bird_function(i, j))
+                z.append(mountain_function(i, j))
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -54,5 +47,5 @@ class MishraBirdMountain:
 
 
 if __name__ == "__main__":
-    mountain = MishraBirdMountain(1, 23)
+    mountain = RandomMountain(10, 10)
     mountain.plot_mountain()
