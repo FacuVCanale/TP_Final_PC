1d979455a364155accf3900395c1a5cb42fac197
Juanfra <djfran774@gmail.com>
2023-06-05 20:11:15 -0300

    SERVER ANDANDO

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-from communication.server.server import server
-server.start()
+from communication.server.server import MountainServer
+from communication.server.mountain.easy_mountain import EasyMountain
+s = MountainServer(EasyMountain(1,10),(14000,14000),50)
+s.start()
