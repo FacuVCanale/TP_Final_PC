9f540926a0c4fa11e1290b3c6a8260b7a5180723
Luqui <playlucas273@gmail.com>
2023-06-09 15:28:42 -0300

    v2
    bastante al azar

--- wip.py
+++ wip.py
@@ -1,50 +1,68 @@
-import math
 import random
-import matplotlib.pyplot as plt
-import numpy as np
+import math
+from typing import Tuple
 
-radius = 23000
-num_points = 100
-points = []
+from communication.server.mountain.circularbase_mountain import CircularBaseMountain
 
-for _ in range(num_points):
-    difx = random.uniform(-1, 1)
-    dify = random.uniform(-1, 1)
-    x = radius * difx
-    y = radius * dify
-    
-    # Generar valores de z utilizando diferentes funciones y escalares aleatorios
-    z = 0
-    num_functions = random.randint(3, 7)  # Cantidad aleatoria de funciones
-    
+class RandomMountain(CircularBaseMountain):
+    def __init__(self, visual_radius, base_radius) -> None:
+        super().__init__(random_function_creator(base_radius), random_gradient_function_creator(base_radius), visual_radius, base_radius)
+        self.flag = self.find_global_max()
+
+    def find_global_max(self):
+        step = 0.1
+        max_value = float('-inf')
+        max_point = (0, 0)
+        for x in range(-self.base_radius, self.base_radius + 1, step):
+            for y in range(-self.base_radius, self.base_radius + 1, step):
+                value = self.function(x, y)
+                if value > max_value:
+                    max_value = value
+                    max_point = (x, y)
+        return max_point
+
+
+def random_function_creator(base_radius):
+    num_functions = random.randint(1, 5)  # Número aleatorio de funciones que componen la montaña
+    functions = []
     for _ in range(num_functions):
-        function_type = random.randint(1, 3)  # Seleccionar aleatoriamente el tipo de función
-        
-        if function_type == 1:
-            scalar = random.uniform(0.5, 2.0)  # Escalar aleatorio
-            z += scalar * math.sin(x / radius * random.uniform(1.0, 3.0) * math.pi)  # Función seno
-            
-        elif function_type == 2:
-            scalar = random.uniform(10, 50)  # Escalar aleatorio
-            z += scalar * math.cos(y / radius * random.uniform(1.0, 3.0) * math.pi)  # Función coseno
-            
-        else:
-            scalar = random.uniform(1, 5)  # Escalar aleatorio
-            z += scalar * math.sin(x / radius * random.uniform(1.0, 3.0) * math.pi) * math.cos(y / radius * random.uniform(1.0, 3.0) * math.pi)  # Producto de funciones
-            
-    z += random.randint(20, 250)  # Valor constante añadido
+        # Generar coeficientes aleatorios para cada función
+        coefficients = [random.uniform(-10, 10) for _ in range(6)]
+        functions.append(coefficients)
     
-    point = [x, y, z]
-    points.append(point)
+    def random_function(x: float, y: float) -> float:
+        x = x / base_radius * 5
+        y = y / base_radius * 5
+        x = x - 5
+        y = y - 5
+        f = 0
+        for coefficients in functions:
+            a, b, c, d, e, f_const = coefficients
+            f += a * math.sin(b * (x + c)) + d * math.cos(e * (y + f_const))
+        return -f
 
-# Extraer las coordenadas x, y, y los valores z
-x_coords = [point[0] for point in points]
-y_coords = [point[1] for point in points]
-z_values = [point[2] for point in points]
+    return random_function
 
-plt.scatter(x_coords, y_coords, c=z_values, cmap="BrBG_r")
-plt.colorbar()
-plt.title("Scatter Plot of (x, y) Coordinates")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show()
+
+def random_gradient_function_creator(base_radius):
+    num_functions = random.randint(1, 5)  # Número aleatorio de funciones que componen la montaña
+    functions = []
+    for _ in range(num_functions):
+        # Generar coeficientes aleatorios para cada función
+        coefficients = [random.uniform(-10, 10) for _ in range(6)]
+        functions.append(coefficients)
+    
+    def random_function_gradient(x: float, y: float) -> Tuple[float, float]:
+        x = x / base_radius * 5
+        y = y / base_radius * 5
+        x = x - 5
+        y = y - 5
+        dfdx = 0
+        dfdy = 0
+        for coefficients in functions:
+            a, b, c, d, e, f_const = coefficients
+            dfdx += a * b * math.cos(b * (x + c))
+            dfdy += -d * e * math.sin(e * (y + f_const))
+        return -dfdx, -dfdy
+
+    return random_function_gradient
