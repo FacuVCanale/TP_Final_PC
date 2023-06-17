c609b388fe3c59978334662da79776771b782f1e
FacuVCanale <facundovcanale@gmail.com>
2023-06-17 18:56:43 -0300

    MOMENTUM GRADIENT
    asd

---
+++ cliente.py
@@ -0,0 +1,41 @@
+from communication.client.client import MountainClient
+import math
+
+MAX_SPEED = 50  # learning rate
+MOMENTUM = 0.5
+momentum = [0, 0] # momentum, beta
+
+c = MountainClient('34.16.147.147', 8080)
+c.add_team('LIFFT', ['FACU', 'FRAN', 'IVAN', 'LUQUI'])
+c.finish_registration()
+while not c.is_over():
+    directions_lifft = {'FACU': {'speed': 50, 'direction': 0},
+                        'FRAN': {'speed': 50, 'direction': 0},
+                        'IVAN': {'speed': 50, 'direction': 0},
+                        'LUQUI': {'speed': 50, 'direction': 0}}
+    data = c.get_data()
+    for climber in data["LIFFT"]:
+        momentum[0] = MOMENTUM * momentum[0] + climber['inclinacion_x']
+        momentum[1] = MOMENTUM * momentum[1] + climber['inclinacion_y']
+        
+        x_new, y_new = climber['x'] - MAX_SPEED * momentum[0], climber['y'] - MAX_SPEED * momentum[1]
+
+        delta_x = x_new - climber['x']
+        delta_y = y_new - climber['y']
+
+        angle = math.atan2(delta_x, delta_y) * (180/math.pi)
+        print(angle)
+
+        directions_lifft[climber]['direction'] = angle
+
+    c.next_iteration('LIFFT', directions_lifft)
+    print(data)
+        
+    
+    
+
+{'T1': 
+ {
+'E1': 
+  {'x': 14000, 'y': 14000, 'z': 3134.8468809073725, 'inclinacion_x': -185.1304347826087, 'inclinacion_y': -181.1304347826087, 'cima': False}, 
+'E2': {'x': 14000, 'y': 14000, 'z': 3134.8468809073725, 'inclinacion_x': -185.1304347826087, 'inclinacion_y': -181.1304347826087, 'cima': False}}, 'T2': {'E1': {'x': 14000, 'y': 14000, 'z': 3134.8468809073725, 'inclinacion_x': -185.1304347826087, 'inclinacion_y': -181.1304347826087, 'cima': False}, 'E2': {'x': 14000, 'y': 14000, 'z': 3134.8468809073725, 'inclinacion_x': -185.1304347826087, 'inclinacion_y': -181.1304347826087, 'cima': False}}}
--- plt_prueba.py
+++ plt_prueba.py
@@ -1,35 +1,45 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#El nombre del equipo es CopNieve
+# codigo
+from communication.client.client import MountainClient
+from communication.util.logger import logger
+import time
+import random
 
-def mountain_function(x, y):
-    # Componentes sinusoidales con diferentes frecuencias y amplitudes
-    component1 = np.sin(0.5 * x + 0.5 * y) * 2
-    component2 = np.sin(2 * x - y) * 1.5
-    component3 = np.sin(3 * x + 0.5 * y) * 3
+cliente = MountainClient("34.16.147.147",8080)
 
-    return component1 + component2 + component3
 
-# Crear una cuadrícula de puntos en el rango deseado
-x_range = np.linspace(-5, 5, 100)
-y_range = np.linspace(-5, 5, 100)
-X, Y = np.meshgrid(x_range, y_range)
+directions ={}
+directions['facu'] = {'speed': 30, 'direction': 45}
+directions['lucas'] = {'speed': 10, 'direction': 50}
 
-# Calcular los valores de z para cada punto en la cuadrícula
-Z = mountain_function(X, Y)
+cliente.add_team("LIFFT",['facu','lucas'])
+    
+cliente.finish_registration()
 
-# Crear una figura en 3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+coord_set = set()
+def mandar_data():
+    while not cliente.is_over():
+        info = cliente.get_data()  # {'LIFFT': {'facu': {'x': 14000, 'y': 14000, 'z': -14109979074.0, 'inclinacion_x': -503966.0, 'inclinacion_y': -503962.0, 'cima': False}}
+        time.sleep(4)
+        print(info)
 
-# Graficar la montaña en 3D
-ax.plot_surface(X, Y, Z, cmap='terrain')
+        cliente.next_iteration("LIFFT", directions)  # VER POR QUE FACU FALLECE Y NO SE PRINTEA EN GET.DATA(), ES DECIR, NO APARECE EN EL SV.
+        with open('coordenadas.tsv', 'a') as file:
+            for team, climbers in info.items():
+                for climber, data in climbers.items():
+                    x = data['x']
+                    y = data['y']
+                    z = data['z']
+                    coord = (x, y, z)
+                    if coord not in coord_set:
+                        line = f"EQUIPO: {team.ljust(10)}\tESCALADOR: {climber.ljust(10)}\tPOSICION: {x}, {y}, {z}\n"
+                        file.write(line)
+                        coord_set.add(coord)
 
-# Personalizar los ejes y el aspecto visual
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Altura')
-ax.set_title('Representación 3D de una montaña con picos diferentes')
+        directions['facu']['direction'] += random.choice([i for i in range(300)])
+        directions['lucas']['direction'] += random.choice([i for i in range(200)])
 
-# Mostrar la figura
-plt.show()
+mandar_data()
+data = cliente.get_data()
+print(data)
+
