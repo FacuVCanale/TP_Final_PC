3b27eda96fcf08cc0d6e4ca3690f874eab288326
Juanfra <djfran774@gmail.com>
2023-06-21 15:37:52 -0300

    a

--- graf.py
+++ graf.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 from communication.client.client import MountainClient
-
+""" a """
 
 res = 50
 
