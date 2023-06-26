5607ec964041d4bc73b4bf921343ad199d2b519f
ivandda <ivandominguezdealzaga@gmail.com>
2023-06-26 02:25:27 -0300

    move definition of strategy function outside the loop

--- test_estrategia2.py
+++ test_estrategia2.py
@@ -38,14 +38,70 @@ c.finish_registration()
 
 
 # Definir Puntos y Estado de cada escalador
-puntos_ivan = [(13500,13500),(14000,14000)]
+puntos_ivan = [(12000,12000),(14000,14000)]
 estado_ivan = 'buscar_punto'
 
+# Esto es la estrategia:----------------------------------------------
+def strategy(hiker,list_of_points,estado):
+    global puntos_ivan
+    global estado_ivan
+
+    estado = estado_ivan
+    puntos_ivan = puntos_ivan
+
+    # tolerancia econtrar punto
+    n = 50 
+    # tolerancia derivada parcial
+    n2 = 0.001
+    
+    # check len of list
+    next_point = list_of_points[0]
+
+    x = hiker.get_data('x')
+    y = hiker.get_data('y')
+    z = hiker.get_data('z')
+
+    dx = hiker.get_data('inclinacion_x')
+    dy = hiker.get_data('inclinacion_y')
+
+
+    if estado == 'buscar_punto':
+        # ver en num negativos
+        if  (x > (next_point[0] - n) and x < (next_point[1] + n)) \
+            and (y > (next_point[0] - n) and y < (next_point[1] + n)):
+            print("Estoy en el punto", x, y)
+            
+            puntos_ivan = puntos_ivan[1:]
+            estado_ivan = 'escalar'
+
+            direction = 0
+            speed = 0
+        
+        else:
+            direction = hiker.direction_p(next_point)
+            speed = hiker.speed_p(next_point)
+            print("buscando punto")
+
+
+    elif estado == 'escalar':
+        
+        if abs(dx) < n2 and abs(dy) < n2:
+            print('estoy en un max local')
+            estado_ivan = 'buscar_punto'
+            direction = 0
+            speed = 0
+        else:
+            print("Escalando")
+            direction = hiker.direction_GA()
+            speed = hiker.speed_GA()
+
+    return direction,speed
+# -----------------------------------------------------------
 
 # Instructions
 while not c.is_over():
     # Sleep server for testing
-    time.sleep(0)
+    time.sleep(0.5)
 
     # Ask for data of all hikers in map
     data = c.get_data()
@@ -57,70 +113,11 @@ while not c.is_over():
     # Print usefull data of DataAnalyst
     dataAnalyst_info = dataAnalyst.get_all_info()
     print("\n DataAnalyst Info = ", dataAnalyst_info)
-  
-
-    # ------------------Codigo de prueba: Aca va la estrategia------------------
-
+ 
+#  Prints para testear
     print("GLOBAL",estado_ivan)
     print("GLOBAL",puntos_ivan)
     
-    # Esto es la estrategia:
-
-    def strategy(hiker,list_of_points,estado):
-        global puntos_ivan
-        global estado_ivan
-
-        estado = estado_ivan
-        puntos_ivan = puntos_ivan
-
-        # tolerancia econtrar punto
-        n = 50 
-        # tolerancia derivada parcial
-        n2 = 0.001
-        
-        # check len of list
-        next_point = list_of_points[0]
-
-        x = hiker.get_data('x')
-        y = hiker.get_data('y')
-        z = hiker.get_data('z')
-
-        dx = hiker.get_data('inclinacion_x')
-        dy = hiker.get_data('inclinacion_y')
-
-
-        if estado == 'buscar_punto':
-            # ver en num negativos
-            if  (x > (next_point[0] - n) and x < (next_point[1] + n)) \
-                and (y > (next_point[0] - n) and y < (next_point[1] + n)):
-                print("Estoy en el punto", x, y)
-                
-                puntos_ivan = puntos_ivan[1:]
-                estado_ivan = 'escalar'
-
-                direction = 0
-                speed = 0
-            
-            else:
-                direction = hiker.direction_p(next_point)
-                speed = hiker.speed_p(next_point)
-                print("buscando punto")
-
-
-        elif estado == 'escalar':
-            
-            if abs(dx) < n2 and abs(dy) < n2:
-                print('estoy en un max local')
-                estado_ivan = 'buscar_punto'
-                direction = 0
-                speed = 0
-            else:
-                print("Escalando")
-                direction = hiker.direction_GA()
-                speed = hiker.speed_GA()
-
-        return direction,speed
-
     directives = {
                     lucas.name: {'direction': lucas.direction_p((988,18245)), 'speed': lucas.speed_p((988,18245))},
                     facu.name: {'direction': facu.direction_p((988,18245)), 'speed': facu.speed_p((988,18245))},
