c0a5e825ea0d9987ea7f669a8f5eb986125b47ec
FacuVCanale <facundovcanale@gmail.com>
2023-06-27 00:45:59 -0300

    grafcador ascii y prueba de 2d

---
+++ leaderboard.py
@@ -0,0 +1,24 @@
+from communication.client.client import MountainClient
+from classes.partida import Partida
+from tabulate import tabulate
+import time
+import os
+
+cliente1 = MountainClient("localhost", 8080)
+
+def mandar_data():
+    count = 1
+    output = ""
+    info = cliente1.get_data()
+    if len(info) > 0:
+        match = Partida(info)
+        teams = match.get_teams()
+        players = []
+        for team in teams:
+            for player in team.get_players():
+                players.append(player)
+        players.sort()
+        player_names = [[count, player.name] for count, player in enumerate(players, start=1)]
+        output += tabulate(player_names, headers=['#', 'Nombre'], tablefmt='fancy_grid') + "\n\n"
+    return output
+
