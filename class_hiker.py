4505393b83f5097f5946217f2e7f0015e7059f21
ivandda <ivandominguezdealzaga@gmail.com>
2023-06-27 17:09:44 -0300

    Add docstrings to class hikers (Work in progress)

--- class_hiker.py
+++ class_hiker.py
@@ -1,10 +1,11 @@
-from communication.client.client import MountainClient
 import numpy as np
 import math
 import facu_inter
 
-# Calss for hikers of our team
 class Hiker:
+    """
+    Class for hikers of our team
+    """
     def __init__(self, team:str, name:str, puntos:list, strat:str="follow_points", alpha:float = 0.1, beta:float = 0.8, alpha2:float = 0.1):
         self.team = team
         self.name = name
@@ -18,20 +19,56 @@ class Hiker:
         self.alpha2 = alpha2 #learning rate
         
         # Momentum Gradient ascent
-        self.vel_x = 0
-        self.vel_y = 0
+        self.vel_x = 0 #previous vel in x to calculate momentum
+        self.vel_y = 0#previous vel in y to calculate momentum
         self.alpha = alpha #learning rate
         self.beta = beta #momentum
     
     def update_data(self, data):
+        """
+        Updates data of hiker and saves it on attribute self.data
+
+        Parameters
+        ----------
+        data : function
+            function that returns data
+
+        Returns
+        -------
+        None
+        """
         self.data = data[self.team][self.name]
 
-    def get_data(self, choice:str):
+    def get_data(self, choice:str)->float:
+        """
+        Returns a specific hiker data 
+
+        Parameters
+        ----------
+        data : str
+            type of data that is going to be returned
+
+        Returns
+        -------
+        specific data
+        """
         return self.data[choice]
     
     def get_direction_and_vel_to_point(self, xf:float, yf:float) -> tuple[float,float]:
         """
-        le das el punto donde queres ir y te da la direccion y vel para llegar mas rapido (linea recta)
+        Receives the x y coords of a point and returns the direction and vel to get to that point (+- 50)
+        vel is always 50
+        Parameters
+        ----------
+        xf : float
+            x coord of point
+        xy : float
+            y coord of point
+
+        Returns
+        -------
+        tuple
+            tuple with direction and vel to get to next point
         """
         xo = self.get_data('x')
         yo = self.get_data('y')
@@ -50,8 +87,21 @@ class Hiker:
     
     def get_direction_and_vel_to_point_JUSTO(self, xf:float, yf:float) -> tuple[float,float]:
         """
-        le das el punto donde queres ir y te da la direccion y vel para llegar mas rapido (linea recta)
-        llega JUSTO al punto donde se le pide
+        receives the x y coords of a point and returns the direction and vel to ge to that point EXACTLY
+        vel is not always 50 because if distance is less that 50 vel is going to be exact to get to
+        the point
+
+        Parameters
+        ----------
+        xf : float
+            x coord of point
+        xy : float
+            y coord of point
+
+        Returns
+        -------
+        tuple
+            tuple with direction and vel to get to next point
         """
         xo = self.get_data('x')
         yo = self.get_data('y')
@@ -71,13 +121,31 @@ class Hiker:
 
         return v_direc,vel
 
-    def get_next_point_GA(self):
+    def get_next_point_GA(self)-> tuple[float,float]:
+        """
+        Using data on hiker attributes gets the next position to climb mountain 
+        with gradient ascent method
+
+        Returns
+        -------
+        tuple
+            tuple with x coord and y coord of next point to climb mountain
+        """
         x_new = self.get_data('x') + self.get_data('inclinacion_x') * self.alpha2
         y_new = self.get_data('y') + self.get_data('inclinacion_y') * self.alpha2
 
         return x_new,y_new
 
     def get_next_point_MGA(self) -> tuple[float,float]:
+        """
+        Using data on hiker attributes gets the next position to climb mountain 
+        with momentum gradient ascent method
+
+        Returns
+        -------
+        tuple
+            tuple with x coord and y coord of next point to climb mountain
+        """
         vel_x_2 = self.beta * self.vel_x + (-1) * self.get_data('inclinacion_x')
         vel_y_2 = self.beta * self.vel_y + (-1) * self.get_data('inclinacion_y')
         
@@ -89,32 +157,86 @@ class Hiker:
 
         return x_new, y_new
     
-    # ---------------- PUEDE SER GA O MGA ------------------
-    def direction_GA(self):
+
+    def direction_GA(self)-> float:
+        """
+        Faster way to call a function that returns direction to climb mountain 
+        using Gradient ascent
+
+        Returns
+        -------
+        float
+            direction
+        """
         next_point_GA = self.get_next_point_GA()
         return self.get_direction_and_vel_to_point(next_point_GA[0], next_point_GA[1])[0]
 
-    def speed_GA(self):
+    def speed_GA(self)-> float:
+        """
+        Faster way to call a function that returns speed to climb mountain 
+        using Gradient ascent
+        Returns
+        -------
+        float
+            speed
+        """
         next_point_GA = self.get_next_point_GA()
         return self.get_direction_and_vel_to_point(next_point_GA[0], next_point_GA[1])[1]
-    # -------------------------------------------------------
 
-    # ---------------- PUEDE SER GA O MGA ------------------
-    def direction_MGA(self):
+    def direction_MGA(self)-> float:
+        """
+        Faster way to call a function that returns direction to climb mountain 
+        using Momentum Gradient ascent
+        Returns
+        -------
+        float
+            direction
+        """
         next_point_MGA = self.get_next_point_MGA()
         return self.get_direction_and_vel_to_point_JUSTO(next_point_MGA[0], next_point_MGA[1])[0]
 
-    def speed_MGA(self):
+    def speed_MGA(self)-> float:
+        """
+        Faster way to call a function that returns direction to climb mountain 
+        using Momentum Gradient ascent
+        Returns
+        -------
+        float
+            speed
+        """
         next_point_MGA = self.get_next_point_MGA()
         return self.get_direction_and_vel_to_point_JUSTO(next_point_MGA[0], next_point_MGA[1])[1]
-    # -------------------------------------------------------
-   
+
    
     # ---------------- PUEDE SER JUSTO O NO ----------------
-    def direction_p(self, point:tuple[float,float]):
+    def direction_p(self, point:tuple[float,float])->float:
+        """
+        Faster way to call a function that returns direction to go to a point EXACTLY
+        
+        Parameters
+        ----------
+        Point : tuple
+            x and y coord of point       
+        Returns
+        -------
+        float
+            direction
+        """
         return self.get_direction_and_vel_to_point_JUSTO(point[0],point[1])[0]
     
-    def speed_p(self, point:tuple[float,float]):
+    def speed_p(self, point:tuple[float,float])->float:
+        """
+        Faster way to call a function that returns speed to go to a point EXACTLY
+        
+        Parameters
+        ----------
+        Point : tuple
+            x and y coord of point       
+        Returns
+        -------
+        float
+            speed
+        """
         return self.get_direction_and_vel_to_point_JUSTO(point[0],point[1])[1]
     # -------------------------------------------------------
 
@@ -125,9 +247,25 @@ class Hiker:
         return coords, is_same
 
     def strategy(self, GA_o_MGA:str ="GA", n=50, n2=0.01):
-        # tolerancia econtrar punto
-        # tolerancia derivada parcial
+        """
+        Strategy to find global max. 
+
+        1 First go to next point in a list of points
+        2 Then  climb to find a global or local max.
+        3 If a local max was found (not win)- Repeat process
+
+        Parameters
+        ----------
+        x : float
+            x
+
+        Returns
+        -------
+        tuple
+            tuple with next direction and speed hiker has to go to follow strategy
+        """
         
+
         # check len of list
         next_point = self.puntos[0]
 
@@ -179,21 +317,46 @@ class Hiker:
 
         return direction,speed
     
-    def change_strat(self, strat):
+    def change_strat(self, strat:str)->None:
+        """
+        Changes hiker strat
+
+        Parameters
+        ----------
+        strat : str
+            type of strat
+
+        """
         self.strat = strat
 
-    def get_next_point(self):
-        """"
-        returns next point that hiker ig going to go
+    def get_next_point(self)->tuple[float,float]:
+        """
+        Using data on hiker attributes predicts the next position hiker is going to be in
+
+        Returns
+        -------
+        tuple
+            tuple with x coord and y coord of next point
         """
         x_next = self.get_data('x') + self.vel*math.cos(self.direc)
         y_next = self.get_data('y') + self.vel*math.sin(self.direc)
         
         return x_next,y_next
 
-    def check_out_of_bounds(self,radius = 23000):
-        """"
-        checks if hikers next point is out of bounds
+    def check_out_of_bounds(self,radius = 23000)->bool:
+        """
+        Checks if hiker next position is going to bo out of bounds of a circle with
+        radius = radius and center in (0,0)
+
+        Parameters
+        ----------
+        radius : float
+            radius of the circle
+
+        Returns
+        -------
+        bool
+            True if out of bounds False if not
         """
         next_coords = self.get_next_point()
         x_next,y_next = next_coords[0], next_coords[1]
